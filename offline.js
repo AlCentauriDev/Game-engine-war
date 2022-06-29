41859d547726610a93477db7c9fc1c43f6deebb9
AlCentauriDev <107649013+AlCentauriDev@users.noreply.github.com>
2022-06-29 19:07:08 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1656498684,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/target-sheet0.png",
+		"images/particles.png",
+		"media/touch.m4a",
+		"media/touch.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"firebase.js"
+	]
+}
